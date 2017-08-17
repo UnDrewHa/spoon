43f809db9cb169c4d9a6418def1fbad6f68b71ae
Gorokhov Andrey <a.gorokhov@rambler-co.ru>
2017-08-18 01:00:43 +0300

    service pages, person, article slider init

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 //= plugins.js
 $(document).ready(function() {
-    
+    $('[data-slick]').each(function () {
+        var self = $(this);
+        if(self.hasClass('slick-initialized')){
+            return;
+        }
+        var slickConfig = $.parseJSON(self.attr('data-slick'));
+
+        self.slick(slickConfig);
+    });
 });
