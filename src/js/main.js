94c124754cc379eb502a25edc486bd2f23ecbbdc
Gorokhov Andrey <a.gorokhov@rambler-co.ru>
2017-08-20 14:39:17 +0300

    ya-share2, hero styles, slider, article footer

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
             return;
         }
         var slickConfig = $.parseJSON(self.attr('data-slick'));
+        self.on('init', function () {
+          var caption = self.find('.slider-item__caption'),
+            arrows = $('.slider-arrow');
 
+          arrows.css('margin-top', '-' + caption.height() + 'px');
+        });
         self.slick(slickConfig);
     });
 });
